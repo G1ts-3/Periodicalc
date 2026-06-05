c509585c77e755819dea170beb8e0288029b3a9b
lpkkelompok5 <lpkkelompok5@gmail.com>
2026-06-05 18:17:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,9 +150,23 @@ st.write(f"*Massa Atom:* {hasil['Massa Atom']}")
 
 st.divider()
 
-st.subheader("Data 118 Unsur")
+st.subheader("Tabel Periodik")
 
-st.dataframe(
-    df,
-    use_container_width=True
-)
+periodik = [
+    ["H","","","","","","","","","","","","","","","","","He"],
+    ["Li","Be","","","","","","","","","","","B","C","N","O","F","Ne"],
+    ["Na","Mg","","","","","","","","","","","Al","Si","P","S","Cl","Ar"],
+    ["K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr"],
+    ["Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe"],
+    ["Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",""],
+    ["Fr","Ra","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr",""]
+]
+
+for baris in periodik:
+    cols = st.columns(18)
+
+    for i, simbol in enumerate(baris):
+        if simbol:
+            cols[i].button(simbol, use_container_width=True)
+        else:
+            cols[i].write("")
